afc711c8f3f24bb09947dbcca96923ee07d1aa0f
Ahmed Raafat <ar.ud.fw@gmail.com>
2023-01-21 14:18:21 +0200

    update backend deployment with Jaeger

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,5 +1,11 @@
+import logging
 from flask import Flask, render_template, request, jsonify
+from flask_opentracing import FlaskTracing
 from prometheus_flask_exporter import PrometheusMetrics
+from jaeger_client import Config
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
 
 import pymongo
 from flask_pymongo import PyMongo
@@ -7,6 +13,9 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 
 app.config["MONGO_DBNAME"] = "example-mongodb"
 app.config[
@@ -15,6 +24,21 @@ app.config[
 
 mongo = PyMongo(app)
 
+
+def init_tracer(service):
+    logging.getLogger("").handlers = []
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+    config = Config(
+        config={"sampler": {"type": "const", "param": 1,}, "logging": True,},
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer('backend')
+tracing = FlaskTracing(tracer, True, app)
 
 @app.route("/")
 def homepage():
